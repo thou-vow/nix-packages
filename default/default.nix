381756e0b7c0e518df365c3decf13116c5f0382d
thou-vow <thou.vow.etoile@gmail.com>
2025-08-30 03:43:06 -0300

    Change graalvm-oracle_21 to nixpkgs-unstable

--- default/default.nix
+++ default/default.nix
@@ -6,13 +6,13 @@ in {
   # nixpkgs only has 17 and 23.
   graalvm-oracle_21 = let
     src = {
-      "x86_64-linux" = pkgs.fetchurl {
+      "x86_64-linux" = pkgs-unstable.fetchurl {
         hash = "sha256-Z6yFh2tEAs4lO7zoXevRrFFcZQUw7w7Stkx9dUB46CE=";
         url = "https://download.oracle.com/graalvm/21/archive/graalvm-jdk-21.0.7_linux-x64_bin.tar.gz";
       };
     };
   in
-    pkgs.graalvmPackages.graalvm-oracle.overrideAttrs (finalAttrs: {
+    pkgs-unstable.graalvmPackages.graalvm-oracle.overrideAttrs (finalAttrs: {
       version = "21";
       src = src.${system};
     });
