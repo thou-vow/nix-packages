efae9a67670076ffc5a9e1e5d5e11a39205a2ec6
thou-vow <thou.vow.etoile@gmail.com>
2026-03-08 04:30:47 -0300

    Readd nixpkgs-stable flake input

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     systems.url = "github:nix-systems/default";
     treefmt-nix = {
