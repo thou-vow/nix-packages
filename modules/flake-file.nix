f7d930bb3e003c188148eada0edcb1b9703c0182
thou-vow <thou.vow.etoile@gmail.com>
2026-05-06 19:35:22 -0300

    Include missing inputs

--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -52,11 +52,20 @@
         };
       };
       import-tree.url = "github:vic/import-tree";
+      nix-index-database = {
+        url = "github:nix-community/nix-index-database";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      nix-on-droid = {
+        url = "github:nix-community/nix-on-droid";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
       nix-wrapper-modules = {
         url = "github:BirdeeHub/nix-wrapper-modules";
         inputs.nixpkgs.follows = "nixpkgs";
       };
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      nixpkgs-nod.url = "github:NixOS/nixpkgs/88d3861acdd3d2f0e361767018218e51810df8a1";
       nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
       nyx-loner = {
         url = "github:lonerOrz/nyx-loner";
