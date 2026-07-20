1c96ec0347217299c502bd0c607b6c897a318c5c
thou-vow <thou.vow.etoile@gmail.com>
2026-07-20 00:27:08 -0300

    fix(faugus-launcher): update packaging for 2.0.0
    Rewrites the derivation to use `buildPythonApplication` with Meson and
    Ninja. Adds missing GTK4, GStreamer, and Python dependencies, and
    configures wrapping for runtime dependencies.

--- pkgs/faugus-launcher.nix
+++ pkgs/faugus-launcher.nix
@@ -1,22 +1,92 @@
 {
-  faugus-launcher,
+  gobject-introspection,
+  gst_all_1,
+  icoextract,
   lib,
+  libadwaita,
+  libgudev,
+  libmanette,
   lsfg-vk,
+  meson,
+  ninja,
+  nix-update-script,
   python3Packages,
   umu-launcher,
-  version,
+  wrapGAppsHook4,
+  xdg-utils,
   src,
+  version,
 }:
-faugus-launcher.overrideAttrs {
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "faugus-launcher";
+  pyproject = false;
+
   inherit version src;
+
+  nativeBuildInputs = [
+    gobject-introspection
+    meson
+    ninja
+    wrapGAppsHook4
+  ];
+
+  buildInputs =
+    [
+      libadwaita
+      libmanette
+      libgudev
+    ]
+    ++ (with gst_all_1; [
+      gst-plugins-base
+      gst-plugins-good
+      gstreamer
+    ]);
+
+  dependencies = with python3Packages; [
+    dbus-python
+    pillow
+    psutil
+    pygobject3
+    requests
+    vdf
+  ];
 
   postPatch = ''
     substituteInPlace faugus-launcher \
       --replace-fail "/usr/bin/python3" "${python3Packages.python.interpreter}"
 
     substituteInPlace faugus/path_manager.py \
+      --replace-fail "PathManager.user_data('faugus-launcher/umu-run')" "'${lib.getExe umu-launcher}'" \
       --replace-fail "/usr/lib/extensions/vulkan/lsfgvk/lib/liblsfg-vk.so" "${lsfg-vk}/lib/liblsfg-vk.so" \
-      --replace-fail "/usr/lib/liblsfg-vk.so" "${lsfg-vk}/lib/liblsfg-vk.so" \
-      --replace-fail "PathManager.user_data('faugus-launcher/umu-run')" "'${lib.getExe umu-launcher}'"
+      --replace-fail "/usr/lib/liblsfg-vk.so" "${lsfg-vk}/lib/liblsfg-vk.so"
   '';
-}
+
+  dontWrapGApps = true;
+
+  preFixup = ''
+    makeWrapperArgs+=(
+      "''${gappsWrapperArgs[@]}"
+      --suffix PYTHONPATH : "$out/${python3Packages.python.sitePackages}:$PYTHONPATH"
+      --suffix PATH : "${
+      lib.makeBinPath [
+        icoextract
+        umu-launcher
+        xdg-utils
+      ]
+    }"
+    )
+    wrapProgram $out/bin/faugus-launcher ''${makeWrapperArgs[@]}
+  '';
+
+  passthru.updateScript = nix-update-script {};
+
+  meta = {
+    description = "Simple and lightweight app for running Windows games using UMU-Launcher";
+    homepage = "https://github.com/Faugus/faugus-launcher";
+    changelog = "https://github.com/Faugus/faugus-launcher/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [RoGreat];
+    mainProgram = "faugus-launcher";
+    platforms = lib.platforms.linux;
+  };
+})
