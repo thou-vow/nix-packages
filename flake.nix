289a791bda7231b08a9b88565e4c8beab51b9b9f
thou-vow <thou.vow.etoile@gmail.com>
2026-07-21 04:11:03 -0300

    chore(nix): add nix-community cachix cache

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://thou-vow.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "thou-vow.cachix.org-1:X9yN6WSwyoFihH/tOriqxpaJEP3pd43z8UPmfipvoK8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
